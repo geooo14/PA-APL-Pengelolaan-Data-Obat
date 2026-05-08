027cf992e11887934621471f953beafc79e3d9f4
Aswad129 <muhammadajaraswadaswad@gmail.com>
2026-05-08 15:14:31 +0800

    feat: savejson

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <conio.h>
+#include <windows.h>
+#include "json.hpp"
 
-int main() {
+using namespace std;
+using json = nlohmann::json;
+
+json LoadJSON(string filename)
+{
+    ifstream i(filename);
+    json j;
+    i >> j;
+    return j;
+}
+
+void SaveJSON(string filename, json j)
+{
+    ofstream o(filename);
+    o << j.dump(4) << endl;
+}
+
+int main()
+{
     cout << "Hello, World!" << endl;
     return 0;
 }
